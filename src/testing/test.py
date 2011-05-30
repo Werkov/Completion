4351451f8cc8d6e22435b9c26bdf3d2b3075161d
Michal Koutny <xm.koutny@gmail.com>
2011-05-31 01:22:21 +0200

    Improved statistics of keystrokes

--- src/testing/test.py
+++ src/testing/test.py
@@ -3,7 +3,10 @@
 import sys
 import codecs
 import math
+import string
 from collections import deque
+from numpy import mean
+from numpy import var
 
 # tests sentences against model
 # @param Model   model
@@ -11,45 +14,53 @@ from collections import deque
 # @param string  alphabet
 # @param int     S      penalty in keystrokes for writing unknown symbol
 # @param int     C      no. of keystrokes necessary for each symbol
-# return no. of tokens (letters), overall cross entropy and overall keystrokes no.
-def testSentences(model, sentences, alphabet, S, C):
-   entropy = 0
-   keystrokes = 0
+# return no. of tokens (letters), overall cross entropy and overall keystrokes list
+def testSentences(model, sentences, alphabet, S, C, verbose = False):
+   entropy = []
+   keystrokes = []
    tokens = 0
    for sentence in sentences:
       sentence = sentence.strip() #remove trailing NL
       pos = 0
       for char in sentence:
-         prob = model.getCharLogProbability(sentence[0:pos], char)
-	 
-	 pos += 1
-         tokens += 1         
-         entropy += -prob / (math.log(2, 10)) # 10 base log to 2 base
+         prob = model.getCharLogProbability(sentence[0:pos], char)         
+         entropy.append(-prob / (math.log(2, 10))) # 10 base log to 2 base
 
          # calculate no. of keystrokes as order in sorted alhabet
          sortedAlphabet = sorted(alphabet, key = lambda(c): model.getCharProbability(sentence[0:pos], c), reverse = 1)
-         keystrokes += C
+         kst = C
+
+	 
          try:
-            keystrokes += sortedAlphabet.index(char)
+            kst += sortedAlphabet.index(char)
+	    if verbose:
+	       print char.encode("utf-8"), "("+format(10**prob, "f")+"): ", string.join(map(lambda(c): c.encode("utf-8"), sortedAlphabet[0:sortedAlphabet.index(char)+1]), ",")
          except ValueError:
-            keystrokes += S
+            kst += S
+	    print char.encode("utf-8"), "("+format(10**prob, "f")+"): ", "not in input alphabet"
+	    
+	 keystrokes.append(kst)
+	 pos += 1
+	 tokens += 1
 
    return tokens, entropy, keystrokes
    
 # -- Default configuration --
 S = 50
 C = 1 # hitting enter
+verbose = False
 
 lettersL = u"abcdefghijklmnopqrstuvwxyzěščřžýáíéůúťďň"
 lettersU = lettersL.upper()
-interpunction = ",."
+numbers = "0123456789"
+interpunction = ",.?!"
 delimiters = " "
-alphabet = lettersL + lettersU + interpunction + delimiters
+alphabet = lettersL + lettersU + delimiters+ interpunction + numbers
 
 
 # -- Parse command line options and arguments --
 if len(sys.argv) < 3:
-   print "Usage: test.py modelName statsFile file(s) ..."
+   print "Usage: test.py modelName statsFile [-s penalty][-c necessary hits][-a alphabet][-v verbose] file(s) ..."
    sys.exit(1)
 
 modelName = sys.argv[1]
@@ -72,6 +83,8 @@ while len(args) > 0:
       C = int(args.popleft())
    elif arg == "-a":
       alphabet = args.popleft()
+   elif arg == "-v":
+      verbose = True
    else:
       files.append(arg)
 
@@ -82,12 +95,13 @@ if len(files) == 0:
 results = []	#results of single tests (to calculate variance)
 for filename in files:
    f = codecs.open(filename, "r", "utf-8")
-   n, h, k = testSentences(model, f.readlines(), alphabet, S, C)
+   n, h, k = testSentences(model, f.readlines(), alphabet, S, C, verbose)
+
    results.append((n, h, k))
    print "File:", filename
    print "Tokens:", n
-   print "Entropy:", h / n
-   print "Keystrokes:", float(k) / n
+   print "Entropy:", mean(h), "+/-", math.sqrt(var(h))
+   print "Keystrokes:", mean(k), "+/-", math.sqrt(var(k))
    print
    f.close()
 
@@ -95,17 +109,17 @@ for filename in files:
 #statistics:
 sumN, sumH, sumK = 0, 0, 0
 for n, h, k in results:
-   sumN, sumH, sumK = sumN + n, sumH + h/n, sumK + float(k)/n
+   sumN, sumH, sumK = sumN + n, sumH + mean(h), sumK + mean(k)
 
 # average
-avgN, avgH, avgK = 0, 0, 0
-avgN, avgH, avgK = float(sumN) / len(results), sumH / len(results), float(sumK) / len(results)
+avgN = mean([res[0] for res in results])
+avgH = mean([mean(res[1]) for res in results])
+avgK = mean([mean(res[2]) for res in results])
 
 #variance
-tmpN, tmpH, tmpK = 0, 0, 0
-for n, h, k in results:
-   tmpN, tmpH, tmpK = (avgN - n)**2, (avgH - h/n)**2, (avgK - float(k)/n)**2
-varN, varH, varK = tmpN / len(results), tmpH / len(results), tmpK / len(results)
+varN = var([res[0] for res in results])
+varH = var([mean(res[1]) for res in results])
+varK = var([mean(res[2]) for res in results])
 
 print "Overall test results:"
 print "Average tokens: " + str(avgN) + " (variance " + str(varN) +  ")"
