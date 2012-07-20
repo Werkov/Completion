ba084a5ac0974e8af9c7fbc3bc5b522071f21ad1
Michal Koutny <xm.koutny@gmail.com>
2012-07-20 20:38:17 +0200

    python3 portability

--- src/python/self-eval.py
+++ src/python/self-eval.py
@@ -1,43 +1,9 @@
-#!/usr/bin/python3
-#from evaluation.Metrics import QwertyMetric
-#import sys
-#
-#from common.Configuration import ConfigurationBuilder
-#from evaluation.Testing import AutomatedTest
-#from evaluation.Metrics import *
-#
-#configBuilder = ConfigurationBuilder()
-#
-#if len(sys.argv) < 3:
-#    print("Usage: {} config-name test-file(s)".format(sys.argv[0]))
-#    sys.exit(1)
-#
-#for filename in sys.argv[2:]:
-#    config  = configBuilder[sys.argv[1]]
-#    test    = AutomatedTest(open(filename))
-#
-#    test.metrics.append(EntropyMetric(config.sorter.languageModel, config.contextLength))
-#    test.metrics.append(SuggesitionsMetric(config.selector, config.sorter, config.contextLength))
-#    test.metrics.append(QwertyMetric(config.selector, config.sorter, config.filter, config.contextLength))
-#    test.runTest()
-#    test.result()
+#!/usr/bin/env python3
 
 import argparse
 import common.configuration
 import evaluation.Testing
 import evaluation.Metrics
-
-#
-#        config = common.configuration.current
-#
-#        self.txtMain                = ui.Completion.TextEdit(self)
-#        self.txtMain.selector       = config.selector
-#        self.txtMain.contextHandler = config.contextHandler
-#
-#        for filter in config.filterChain:
-#            self.txtMain.addFilter(filter)
-#
-    
 
 def main():
     # initialize own parser
@@ -56,7 +22,7 @@ def main():
     test.metrics.append(evaluation.Metrics.Metric(common.configuration.current))
     test.metrics.append(evaluation.Metrics.PerplexityMetric(common.configuration.current))
     test.metrics.append(evaluation.Metrics.QwertyMetric(common.configuration.current))
-    test.metrics.append(evaluation.Metrics.SelectorMetric(common.configuration.current))
+#    test.metrics.append(evaluation.Metrics.SelectorMetric(common.configuration.current))
 
     
     test.runTest()
